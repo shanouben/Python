6103d13ae8896663a4c2c6150bdab3e86f3dd0ce
shanouben <shanouben@gmail.com>
2022-09-10 12:28:37 +0200

    commit 1

---
+++ Lecture1.py
@@ -0,0 +1,8 @@
+
+import numpy as np
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_excel('https://www.oliviergueant.com/uploads/4/3/0/9/4309511/sbf120_as_of_end_2018.xlsx')
