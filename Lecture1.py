3e25c2bbbc0fa717f8007e0019884ae03fa10c24
shanouben <shanouben@gmail.com>
2022-09-12 09:31:17 +0200

    2eme commit

--- Lecture1.py
+++ Lecture1.py
@@ -1,8 +1,15 @@
 
+from time import time
 import numpy as np
 from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tqdm import tqdm
 
-data = pd.read_excel('https://www.oliviergueant.com/uploads/4/3/0/9/4309511/sbf120_as_of_end_2018.xlsx')
+
+data = pd.read_excel('excel1.xlsx', sheet_name=1)
+print(data)
+
+dt =pd.to_datetime(data)
+print(dt)
